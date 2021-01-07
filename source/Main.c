194c5d5c4aa146357f459c48b99c973f309bb0a4
bluenofire <liu3355@gmail.com>
2021-01-07 19:31:52 +0800

    “sc

--- source/Main.c
+++ source/Main.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-int eat[4] = { 80, 50, 40, 30 };
+
 int drink[4] = { 50, 30, 20, 40 };
 int select[4], flag=1, flag1, flag2, cnt, money1, money2;
 int main()
 {
-
-			printf("主餐:\n");
-			printf("1.炒飯(80元) 請選擇數量:");
+	
+		
+		
+			printf("附餐:\n");
+			printf("1.咖啡(50元) 請選擇數量:");
 			scanf_s("%d", &select[0]);
 			printf("\n");
-			printf("2.漢堡(50元) 請選擇數量:");
+			printf("2.可樂(30元) 請選擇數量:");
 			scanf_s("%d", &select[1]);
 			printf("\n");
-			printf("3.起司堡(40元) 請選擇數量:");
+			printf("3.紅茶(20元) 請選擇數量:");
 			scanf_s("%d", &select[2]);
 			printf("\n");
-			printf("4.薯條(30元) 請選擇數量:");
+			printf("4.奶茶(40元) 請選擇數量:");
 			scanf_s("%d", &select[3]);
 			printf("\n");
 			for (cnt = 0; cnt <= 3; cnt++)
 			{
-				money1 += eat[cnt] * select[cnt];  //money1 = money1 + eat[cnt] * select[cnt];
+				money2 += drink[cnt] * select[cnt];
 			}
-			printf("目前消費:%d元\n", money1);
-			printf("是否點選附餐? 1是 0否:");
-			scanf_s("%d", &flag2);
+			printf("消費總額:%d元\n", money1 + money2);
 		
+	
+	
 	system("PAUSE");
 }
